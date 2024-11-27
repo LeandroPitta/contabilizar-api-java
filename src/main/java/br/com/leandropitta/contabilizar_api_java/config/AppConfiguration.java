008b9568028e4081ffeba6fc6fded612a197412d
Leandro Pitta <lspitta@gmail.com>
2024-11-27 10:04:45 -0300

    Incluido no AppConfiguration o addCorsMappings.

--- src/main/java/br/com/leandropitta/contabilizar_api_java/config/AppConfiguration.java
+++ src/main/java/br/com/leandropitta/contabilizar_api_java/config/AppConfiguration.java
@@ -3,12 +3,23 @@ package br.com.leandropitta.contabilizar_api_java.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements WebMvcConfigurer {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
