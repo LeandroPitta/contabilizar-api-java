78e03a37e35a9c4702352466c9d1da5a7b35d001
Leandro Pitta <lspitta@gmail.com>
2024-12-04 16:00:47 -0300

    Autorizando qualquer origem no cors.

--- src/main/java/br/com/leandropitta/contabilizar_api_java/config/AppConfiguration.java
+++ src/main/java/br/com/leandropitta/contabilizar_api_java/config/AppConfiguration.java
@@ -17,7 +17,7 @@ public class AppConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
